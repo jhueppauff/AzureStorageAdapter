7a76c48d64318b637e994cc3d09e41538605aff2
Julian Hüpppauff <julian@hueppauff.info>
2018-10-12 22:51:30 +0200

    initial

---
+++ AzureStorageAdapter/BlobStorageAdapter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureStorageAdapter
+{
+    public class BlobStorageAdapter
+    {
+        private readonly CloudBlobContainer libraryContainer;
+
+        public BlobStorageAdapter(string blobConnectionString, string containerName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(blobConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            libraryContainer = blobClient.GetContainerReference(containerName);
+        }
+
+        public Task<string> UploadToBlob(byte[] data, string name, string contentType, bool overwrite = false)
+        {
+            return UploadToBlob(new MemoryStream(data), name, contentType, overwrite);
+        }
+
+        public Task<string> UploadToBlob(string data, string name, string contentType, bool overwrite = false)
+        {
+            return UploadToBlob(new MemoryStream(Convert.FromBase64String(data)), name, contentType, overwrite);
+        }
+
+        private async Task<string> UploadToBlob(Stream stream, string name, string contentType, bool overwrite = false)
+        {
+            CloudBlockBlob blockBlob = libraryContainer.GetBlockBlobReference(name);
+            if (!await blockBlob.ExistsAsync())
+            {
+                await blockBlob.UploadFromStreamAsync(stream);
+
+                blockBlob.Properties.ContentType = contentType;
+                await blockBlob.SetPropertiesAsync();
+            }
+
+            return blockBlob.Uri.ToString();
+        }
+    }
+}
--- AzureStorageAdapter/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AzureStorageAdapter
-{
-    public class Class1
-    {
-    }
-}
