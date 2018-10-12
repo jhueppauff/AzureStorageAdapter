a39b9d64b373563028299d19b491219fcbb54d74
Julian Hüpppauff <julian@hueppauff.info>
2018-10-12 23:21:29 +0200

    added unit test

---
+++ UnitTest/BlobTests.cs
@@ -0,0 +1,50 @@
+using AzureStorageAdapter;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class BlobTests
+    {
+        [TestMethod]
+        public async Task TestBlobUpload()
+        {
+            string sample = System.IO.Directory.GetParent(AppContext.BaseDirectory).FullName + @"\Resources\sample.txt";
+
+            var configuration = GetConfiguration(); 
+            BlobStorageAdapter blobStorageAdapter = new BlobStorageAdapter(configuration.GetSection("AzureBlogStorage:BlobConnectionString").Value, configuration.GetSection("AzureBlogStorage:BlobContainer").Value);
+            string uri = await blobStorageAdapter.UploadToBlob(await File.ReadAllBytesAsync(sample), Guid.NewGuid() + ".txt", "text/plain", true);
+
+            Assert.IsTrue(!String.IsNullOrWhiteSpace(uri));
+
+            string path = System.IO.Directory.GetParent(AppContext.BaseDirectory).FullName + @"\download.txt";
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(uri, path);
+            }
+
+            Assert.IsTrue(File.Exists(path));
+            var fileInfo = new FileInfo(path);
+
+            Assert.IsTrue(fileInfo.Length != 0);
+        }
+
+        private IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false).Build();
+        }
+    }
+}
