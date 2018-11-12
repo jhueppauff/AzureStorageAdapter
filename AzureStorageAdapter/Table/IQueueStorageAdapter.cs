57a55f6fc463c46b5658e993251f12e1d87cb03d
Julian Hüpppauff <julian@hueppauff.info>
2018-11-12 20:48:19 +0100

    added base functions

---
+++ AzureStorageAdapter/Table/IQueueStorageAdapter.cs
@@ -0,0 +1,27 @@
+﻿namespace AzureStorageAdapter.Table
+{
+    using Microsoft.WindowsAzure.Storage.Queue;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    interface IQueueStorageAdapter
+    {
+        Task CreateQueueAsync(string name);
+
+        Task AddEntryToQueueAsync(string queueName, string messageText);
+
+        Task<string> PeekNextMessageStringAsync(string queueName);
+
+        Task<CloudQueueMessage> PeekNextMessageAsync(string queueName);
+
+        Task ChangeMessageContentAsync(string queueName, string messageContent);
+
+        Task RemoveNextMessageAsync(string queueName);
+
+        Task<int> GetQueueLengthAsync(string queueName);
+
+        Task DeleteQueueAsync(string queueName);
+    }
+}
