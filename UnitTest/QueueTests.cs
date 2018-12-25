13113bc6c088ae97df8940ca37daf15b4e01d696
Julian Hüpppauff <julian@hueppauff.info>
2018-12-25 08:48:09 +0100

    added QueueTest

---
+++ UnitTest/QueueTests.cs
@@ -0,0 +1,50 @@
+﻿namespace UnitTest
+{
+    using AzureStorageAdapter.Queue;
+    using FluentAssertions;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class QueueTests
+    {
+        private IConfiguration configuration;
+        private const string QueueName = "test";
+        private QueueStorageAdapter queue;
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
+            this.configuration = this.GetConfiguration();
+            this.queue = new QueueStorageAdapter(this.configuration.GetSection("AzureBlogStorage:BlobConnectionString").Value);
+            await this.queue.CreateQueueAsync(QueueName);
+        }
+
+        [TestMethod]
+        public async Task QueueMessage()
+        {
+            await this.queue.AddEntryToQueueAsync(QueueName, "test");
+
+            var message = await this.queue.PeekNextMessageAsync(QueueName);
+
+            message.AsString.Should().Equals("test");
+        }
+
+        [TestCleanup]
+        public async Task Dispose()
+        {
+            await this.queue.DeleteQueueAsync(QueueName);
+            this.queue = null;
+            this.configuration = null;
+        }
+
+        private IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false).Build();
+        }
+    }
+}
