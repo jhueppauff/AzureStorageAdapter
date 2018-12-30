7cf6deea6dae1ac822445ec260407c39187866a7
Julian Hüpppauff <julian@hueppauff.info>
2018-12-30 19:55:03 +0100

    added base unit test for queue

---
+++ UnitTest/QueueTests.cs
@@ -0,0 +1,35 @@
+﻿namespace UnitTest
+{
+    using System.Threading.Tasks;
+    using AzureStorageAdapter.Queue;
+    using FluentAssertions;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class QueueTests
+    {
+        private const string queueName = "sdktest";
+        private QueueStorageAdapter queueStorageAdapter;
+        private IConfiguration configuration;
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
+            this.configuration = Configuration.GetConfiguration();
+            this.queueStorageAdapter = new QueueStorageAdapter(configuration.GetSection("AzureBlogStorage:BlobConnectionString").Value);
+            await this.queueStorageAdapter.CreateQueueAsync(queueName);
+        }
+
+        [TestMethod]
+        public async Task AddEntry()
+        {
+            await queueStorageAdapter.AddEntryToQueueAsync(queueName, "test").ConfigureAwait(false);
+
+            var message = await queueStorageAdapter.PeekNextMessageAsync(queueName).ConfigureAwait(false);
+
+            message.AsString.Should().Equals("test");
+        }
+
+    }
+}
