ed3a309926807861042e67761fc7be525d6d0f6b
Julian Hüpppauff <julian@hueppauff.info>
2019-01-16 19:59:46 +0100

    removed preceded black line

--- AzureStorageAdapter/Table/TableStorageAdapter.cs
+++ AzureStorageAdapter/Table/TableStorageAdapter.cs
@@ -86,7 +86,6 @@ namespace AzureStorageAdapter.Table
 
                 await cloudTable.ExecuteBatchAsync(batchOperation).ConfigureAwait(false);
             }
-
         }
 
         /// <summary>
